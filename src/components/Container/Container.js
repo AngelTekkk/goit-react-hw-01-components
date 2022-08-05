3baf04905c486ed0d9c23ab4de78e08fe3a1d616
Andriy Makarov <angeltek69@gmail.com>
2022-08-05 19:23:36 +0200

    Fulfilled

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,7 +1,12 @@
+import PropTypes, { element } from 'prop-types';
 import s from 'components/Container/Container.module.css';
 
 function Container({ children }) {
   return <div className={s.container}>{children}</div>;
 }
+
+Container.propTypes = {
+  children: PropTypes.array,
+};
 
 export default Container;
